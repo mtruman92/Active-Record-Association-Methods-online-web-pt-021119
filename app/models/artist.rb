10f47c8917819a81d0cc1da713eeae8babbca8d3
Mickae Truman <mtruman@nyit.edu>
2019-05-14 01:36:21 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    Artist.songs.genres.first
+    Artist.songs.first.genres
     #return the genre of the artist's first saved song
   end
 
