26bbccafd9d9ad5f33a1479fc0a21aff652d6ec4
Mickae Truman <mtruman@nyit.edu>
2019-05-14 01:48:41 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,8 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists
+    self.artists.collect do |artist|
+      artist.name
     # return an array of strings containing every musician's name
   end
 end
